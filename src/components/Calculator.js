87b1d85cda18b6d3a777a3144ce04c9a568b21b3
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-11-29 16:00:10 +0300

    calculator design

---
+++ src/components/Calculator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="view-num">
+          <span>0</span>
+        </div>
+        <div className="buttons-container">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="special-button">/</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="special-button">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="special-button">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="special-button">+</button>
+          <button type="button" className="zero">0</button>
+          <button type="button">.</button>
+          <button type="button" className="special-button">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Calculator;
