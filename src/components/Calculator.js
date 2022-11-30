6bcc5aa9d87cb247a1915a2c80db4e7dc1114ab5
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-11-30 12:43:19 +0300

    adding the logic to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="view-num">
-          <span>0</span>
-        </div>
-        <div className="buttons-container">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="special-button">/</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="special-button">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="special-button">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="special-button">+</button>
-          <button type="button" className="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="special-button">=</button>
-        </div>
+const Calculator = () => {
+  let obj = {};
+  const [result, setResult] = useState(obj);
+  const handleClick = (e) => {
+    const buttonValue = e.target.textContent;
+    obj = calculate(result, buttonValue);
+    setResult(obj);
+  };
+
+  return (
+    <div>
+      <div className="view-num">
+        <span>{result.next || result.operation || result.total || 0}</span>
       </div>
-    );
-  }
-}
+      <div className="buttons-container">
+        <button type="button" onClick={handleClick}>AC</button>
+        <button type="button" onClick={handleClick}>+/-</button>
+        <button type="button" onClick={handleClick}>%</button>
+        <button type="button" onClick={handleClick} className="special-button">&#xf7;</button>
+        <button type="button" onClick={handleClick}>7</button>
+        <button type="button" onClick={handleClick}>8</button>
+        <button type="button" onClick={handleClick}>9</button>
+        <button type="button" onClick={handleClick} className="special-button">x</button>
+        <button type="button" onClick={handleClick}>4</button>
+        <button type="button" onClick={handleClick}>5</button>
+        <button type="button" onClick={handleClick}>6</button>
+        <button type="button" onClick={handleClick} className="special-button">-</button>
+        <button type="button" onClick={handleClick}>1</button>
+        <button type="button" onClick={handleClick}>2</button>
+        <button type="button" onClick={handleClick}>3</button>
+        <button type="button" onClick={handleClick} className="special-button">+</button>
+        <button type="button" onClick={handleClick} className="zero">0</button>
+        <button type="button" onClick={handleClick}>.</button>
+        <button type="button" onClick={handleClick} className="special-button">=</button>
+      </div>
+    </div>
+  );
+};
 export default Calculator;
